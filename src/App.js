136107e7a7234cc8919241c763d22e525b770527
myeongwooni <uriuni17@naver.com>
2021-07-03 03:54:26 +0900

    1.1 Setup - Themes on Styled Components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,26 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound  from "./screens/NotFound";
-import {isLoggedInVar} from "./apollo";
+import {isLoggedInVar, darkModeVar} from "./apollo";
+import {ThemeProvider} from "styled-components";
+
+const lightTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "lightgray",
+};
+
+const darkTheme = {
+  fontColor: "#lightgray",
+  bgColor: "#2c2c2c",
+};
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Router>
-        <Switch>
+          <Switch>
           <Route path="/" exact>
             {isLoggedIn ? <Home/> : <Login/>}
           </Route>
@@ -20,7 +32,7 @@ function App() {
           </Route>
         </Switch>
       </Router>
-    </div>
+    </ThemeProvider>
   );
 }
 
